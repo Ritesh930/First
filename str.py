67a672467c4faa7611e7c81af45f115ec2259be9
Ritesh930 <yadavritesh930622@gmail.com>
2025-06-12 09:47:15 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,42 @@
+# string is immutable-- cannot be changed
+# denoted by -- single ,double and triple quotes
+#name="Ritesh yadav"
+# print(type(name))
+# print(name)
+
+#acessing methods of string
+name="Ritesh yadav"
+# print(name.upper())  #uppercase
+# print(name.lower())  #lowercase
+# print(name.title())  #title format
+# print(name.capitalize()) #first letter uppercase rest lowercase
+# print(name.casefold()) # make all lowercase
+# print(name.swapcase()) #converts uppercase into lower and vice versa
+# print(name.strip())  #remove white space
+# print(name.lstrip()) #remove from left
+# print(name.rstrip())  #remove from right
+# print(name.replace("Ritesh","Divya"))  #replace with old 
+# print(name.split(" ")) #split from , no given split place is displayed
+# print(name.partition(" ")) #use to part and display the given partition word
+# print(name.startswith("R"))  #true
+# print(name.endswith("v"))    #true
+# print(name.encode("ascii")) #use to encode 
+# print(name.zfill(20)) #set string lenth by adding zero in front
+# print(name.center(20, "^"))
+# print(name.ljust(20, "^"))
+# print(name.rjust(20, "^"))
+
+   #indexing and slicing
+#indexing-- accessing single character from the string
+#slicing-- accessing multiple character from the string
+# print(name[2]) #print t index
+# print(name[-2]) #print a from back side index
+# print(name[0:7]) #print Ritesh 
+
+# age=10        #f is used to print values like this , it is a formatted string.
+# text=f"my age is{age}"
+# print(text)
+
+# text="""my name is 'Ritesh yadav'""" #to add quotes while printing string
+# print(text)
+
